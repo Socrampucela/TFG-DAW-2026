be59c2283932c9b24284f69aa2c2718f5783ca5d
AlejandroMRdaw <alejandro.morrod.1@educa.jcyl.es>
2026-01-27 17:55:25 +0100

    formulario y registro

--- AUTH/register.php
+++ AUTH/register.php
@@ -4,12 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
-    <link rel="stylesheet" href="ASSETS/CSS/.css">
+    <link rel="stylesheet" href="ASSETS/css/register.css">
 </head>
 <body>
     <h1>Regístrate</h1>
-    <form action="#" method="post">
-        <input type="text" name="nombre" placeholder="Introduce tu nombre" id="">
+    <form action="procesarRegistro.php" method="post">
+        <div>
+            <label for="nombre">Nombre y apellidos</label><br>
+            <input type="text" id="nombre" name="nombre" required>
+        </div>
+
+        <div>
+            <label for="email">Correo electrónico</label><br>
+            <input type="email" id="email" name="email" required>
+        </div>
+
+        <div>
+            <label for="password">Contraseña</label><br>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div>
+            <label for="password2">Repetir contraseña</label><br>
+            <input type="password" id="password2" name="password2" required>
+        </div>
+
+        <div>
+            <label>
+                <input type="checkbox" name="condiciones" required>
+                Acepto los términos y condiciones
+            </label>
+        </div>
+
+        <button type="submit">Registrarse</button>
     </form>
 </body>
 </html>

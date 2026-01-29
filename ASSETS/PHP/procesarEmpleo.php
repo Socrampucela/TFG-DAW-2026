76c049c3fa8de30985aa7ec75ebfbaa995ac9fd3
Socrampucela <socrampucela85@gmail.com>
2026-01-29 20:19:26 +0100

    Crear empleo y se inserta bien en la base de datos

--- ASSETS/PHP/procesarEmpleo.php
+++ ASSETS/PHP/procesarEmpleo.php
@@ -1,10 +1,26 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $titulo = $_POST["titulo"];
-    $provincia = $_POST["provincia"]; //ID PROVINCIA
-    $localidad = $_POST["localidad"]; //ID LOCALIDAD
-    
-}
-else{
-    header("location:../VIEWS/crearEmpleo.php?error=ErrorFormulario");
+session_start();
+require_once "../../CONFIG/db.php";
+require_once "../../CLASSES/empleo.php";
+require_once "../../DAO/empleoDAO.php"; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['nombre'])) {
+
+    $empleo = new Empleo(
+        $_POST['titulo'],
+        $_POST['provincia'],  
+        $_POST['descripcion'],
+        $_POST['localidad'],   
+        $_POST['enlace']
+    );
+
+    $dao = new EmpleoDAO($conn);
+
+    if ($dao->insertar($empleo)) {
+        header("Location: ../../VIEWS/index.php");
+    } else {
+        header("Location: ../../VIEWS/crearEmpleo.php?error=db_error");
+    }
+} else {
+    header("Location: ../../VIEWS/login.php");
 }
